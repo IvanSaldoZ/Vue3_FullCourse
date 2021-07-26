2b74eae4c2706eea60b1285ca88f75491a9a3f0d
WinPCi9 <saldoz@ya.ru>
2021-07-26 17:41:23 +0300

    Добавлена модуль data > likes и dislikes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
     Работает!
+
+    <div>
+      Количество лайков <strong>{{ likes }}</strong>
+      <br>
+      Количество дизайлков <strong>{{ dislikes }}</strong>
+    </div>
   </div>
 </template>
 
 <script>
+
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5,
+    }
+  }
+}
+
 
 </script>
 
