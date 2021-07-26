d5034331a14afd4235de5cfcdf9e9f2f998fbd3b
WinPCi9 <saldoz@ya.ru>
2021-07-26 20:17:30 +0300

    Реализовано ДОБАВЛЕНИЕ ПОСТОВ в массив постов JavaScript

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Добавление поста</h4>
+      <input
+          v-bind:value="title"
+          @input="title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Описание">
+      <input
+          v-bind:value="body"
+          @input="body = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Название">
+      <button
+          class="btn"
+          @click="createPost">Добавить</button>
+    </form>
     <div class="post" v-for="post in posts">
       <div><strong>Название:</strong> {{ post.title }}</div>
       <div><strong>Описание:</strong> {{ post.body }}</div>
@@ -18,8 +36,22 @@ export default {
         {id: 2, title: 'Какой язык лучше: 2', body: 'Ответ на этот вопрос в этой статье 2'},
         {id: 3, title: 'Какой язык лучше: 3', body: 'Ответ на этот вопрос в этой статье 3'},
       ],
+      title: '',
+      body: ''
     }
   },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost);
+      this.title = "";
+      this.body = "";
+    }
+  }
 }
 
 
@@ -32,9 +64,35 @@ export default {
   box-sizing: border-box;
 }
 
+.app {
+  padding: 20px;
+}
+
 .post {
   padding: 15px;
   border: 2px solid green;
   margin-top: 15px;
 }
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+
 </style>
