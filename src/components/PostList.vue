4da166305e8aa82251d4d71fdffb2612f57e3288
WinPCi9 <saldoz@ya.ru>
2021-07-27 19:24:16 +0300

    Директива v-show для отображения надписи "Список постов пуст" при удалении всех постов из списка

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,16 @@
 <template>
-  <h3>Список постов</h3>
-  <!-- @remove - это on_remove - это событие из дочернего компонента post-item по удалению поста -->
-  <post-item v-for="post in posts"
-             :post="post"
-             :key="post.id"
-             @remove="$emit('remove', post)"
-  />
+  <div v-show="posts.length > 0">
+    <h3>Список постов</h3>
+    <!-- @remove - это on_remove - это событие из дочернего компонента post-item по удалению поста -->
+    <post-item v-for="post in posts"
+               :post="post"
+               :key="post.id"
+               @remove="$emit('remove', post)"
+    />
+  </div>
+  <h2 v-show="posts.length === 0" style="color: red">
+    Список постов пуст!
+  </h2>
 </template>
 
 <script>
