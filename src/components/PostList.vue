fe474f81db421ef931177a63786864e4178407f6
WinPCi9 <saldoz@ya.ru>
2021-07-28 19:18:14 +0300

    Добавлена анимация при добавлении/удалении постов и их сортировке

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,15 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <!-- @remove - это on_remove - это событие из дочернего компонента post-item по удалению поста -->
-    <post-item v-for="post in posts"
-               :post="post"
-               :key="post.id"
-               @remove="$emit('remove', post)"
-    />
+    <!-- Анимация списков: https://v3.ru.vuejs.org/ru/guide/transitions-list.html -->
+    <transition-group name="posts-list">
+      <!-- @remove - это on_remove - это событие из дочернего компонента post-item по удалению поста -->
+      <post-item v-for="post in posts"
+                 :post="post"
+                 :key="post.id"
+                 @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2 v-else style="color: red">
     Список постов пуст!
@@ -27,5 +30,25 @@
 </script>
 
 <style scoped>
+/* https://v3.ru.vuejs.org/ru/guide/transitions-list.html#анимации-добавления-и-удаления-элементов-списка */
+/* Анимация добавления-удаления */
+.posts-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.posts-list-enter-active,
+.posts-list-leave-active {
+  transition: all 0.4s ease;
+}
+.posts-list-enter-from,
+.posts-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+/* Анимация при сортировке */
+.posts-list-move {
+  transition: transform 0.4s ease;
+}
 
 </style>
